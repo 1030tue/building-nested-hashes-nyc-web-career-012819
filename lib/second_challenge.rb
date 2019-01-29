1ef4cd2386a69b7ba0535c3e83f7740609e56975
Kate <katekim5809@gmail.com>
2019-01-29 00:17:39 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,17 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {:patriarch => {}, :matriarch => {}, :hero => {}, :heroine_friends =>[]
+    :montague => {}
+    :patriarch => {},
+    :matriarch => {},
+    :hero => {},
+    :heroine_friends =>[]
       #your key/value pairs here!
       },
-    :capulet => {:patriarch => {}, :matriarch => {}, :hero => {}, :heroine_friends =>[]
+    :capulet => {}
+    :patriarch => {},
+    :matriarch => {},
+    :hero => {},
+    :heroine_friends =>[]
       #your key/value pairs here!
       }}
 
