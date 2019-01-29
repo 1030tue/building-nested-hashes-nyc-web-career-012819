8e25313cd7f52f8b4611f23f23df97469133e112
Kate <katekim5809@gmail.com>
2019-01-29 00:11:32 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -7,9 +7,9 @@ def fourth_challenge
       :hero_friends => [
         #your hashes here!
         {name: "Mercutio", age: "18", attitude: "hot-headed"}
-        
+
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -22,6 +22,6 @@ def fourth_challenge
    }
 }
 
-  
+
 
 end
